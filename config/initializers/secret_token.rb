e64b56c83f2bf3633b4f24ff9fceef8639dab36f
Evan Derby <somekidwithhtml@gmail.com>
2014-04-18 00:02:27 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FusionDigitalRails::Application.config.secret_key_base = '99ee90e1e7bbe640ccafcbc1aeb08476fdf82996317b6062707ec5a59aa8916fe75385a740b7399166eea9a28b50525934a4b872efd1107f229d442036a8ac8f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FusionDigitalRails::Application.config.session_store :cookie_store, key: '_fusion-digital-rails_session'
