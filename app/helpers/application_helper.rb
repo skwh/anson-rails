5e1fda18259efd593710d494720c7a3d5274669b
Evan Derby <somekidwithhtml@gmail.com>
2014-04-20 19:40:55 -0600

    Added project and static styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,16 @@ module ApplicationHelper
 			"#{base_title} | #{page_title}"
 		end
 	end
+
+	def current_page(fullpath) 
+		if fullpath == "/projects"
+			1
+		elsif fullpath == "/about"
+			2
+		elsif fullpath == "/contact"
+			3
+		else
+			4
+		end
+	end
 end
