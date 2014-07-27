6abbda30fd244b7c159aef02da3986e2f97eb1c1
Evan Derby <somekidwithhtml@gmail.com>
2014-07-27 14:12:07 -0600

    Fixed one slide arrows on static pages

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -2,30 +2,26 @@
 // All this logic will automatically be available in application.js.
 var ready;
 ready = function() {
+	var slider_div = $("div[id*='slider']")[0];
+	var slides_div = $("div[u='slides']")[0];
+	var jssor_slider;
+	var body_width = $('#general_body').getStyleObject().width;
+	slider_div.style.width = body_width;
+	slides_div.style.width = body_width;
 	//setup the options for the sliders
 	var options = { 
 		$AutoPlay: true,
 		$AutoPlayInterval: 5000,
 		$DragOrientation: 0,
-		$FillMode: 1,
-		$ArrowNavigatorOptions: {
-			$Class: $JssorArrowNavigator$,
-			$ChanceToShow: 2
-		}
+		$FillMode: 1
 	};
-	//get the slider that exists on the page
-	var slider_div = $("div[id*='slider']")[0];
-	//get the divs that contain the slides
-	var slides_div = $("div[u='slides']")[0];
-	//placeholder for the jssorslider object
-	var jssor_slider;
-	//get the width of the body
-	var body_width = $('#general_body').getStyleObject().width;
-	//set the width of the slider container to its parent, the body div
-	slider_div.style.width = body_width;
-	//set the width of the slides container to the body div
-	slides_div.style.width = body_width;
-	//create the jssor slider object with the current slider's id and the options
+	if ($(slides_div).children().length > 1) {
+		options.$ArrowNavigatorOptions = {
+				$Class: $JssorArrowNavigator$,
+				$ChanceToShow: 2
+		}
+	}
+
 	jssor_slider = new $JssorSlider$('slider', options);
 	//resize the boxes so that the arrows are positioned correctly
 	repositionArrowsAfterResize();
@@ -37,11 +33,13 @@ window.onresize = function(event) {
 
 function repositionArrowsAfterResize() {
 	//correct the positions of the arrows after the window is resized
-	var slider_width = parseInt($('#general_body').getStyleObject().width,10);
-	var slider_height = parseInt($('#slider').getStyleObject().height,10);
-	$('.jssora01r')[0].style.left=(slider_width-53)+"px";
-	$('.jssora01r')[0].style.top = ((slider_height/2)-53)+"px";
-	$('.jssora01l')[0].style.top = ((slider_height/2)-53)+"px";
+	if ($($("div[u='slides']")[0]).children().length > 1) {
+		var slider_width = parseInt($('#general_body').getStyleObject().width,10);
+		var slider_height = parseInt($('#slider').getStyleObject().height,10);
+		$('.jssora01r')[0].style.left=(slider_width-53)+"px";
+		$('.jssora01r')[0].style.top = ((slider_height/2)-53)+"px";
+		$('.jssora01l')[0].style.top = ((slider_height/2)-53)+"px";
+	}
 }
 
 $(document).ready(ready);
